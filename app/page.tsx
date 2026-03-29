19117d10917c5f5f63cb1d5b4d85e012eae1f460
chunchusangeetha <116276304+chunchusangeetha@users.noreply.github.com>
2026-03-29 21:43:13 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main >
-      <h1 className="text-2xl font-bold mb-2">Home Page</h1>
-      <p className="text-gray-600">Welcome to my Next.js learning journey</p>
-    </main>
-  );
-}
